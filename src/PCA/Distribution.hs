c7ea896e5c549f15221da844e5091cc4dde4b916
Danila <ddbihbka@gmail.com>
2015-08-17 10:18:52 +0300

    Add general form of Distribution

---
+++ src/PCA/Distribution.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+module PCA.Distribution where
+
+import Numeric.LinearAlgebra.Data (Vector, Matrix)
+
+
+data family Distr m v
+
+data instance Distr Double Double = Gamma Double Double
+
+data instance
+     Distr (Vector Double) (Vector Double) = MGamma (Vector Double)
+                                                    (Vector Double)
+
+
+data instance
+     Distr (Vector Double) (Matrix Double) = MNormal (Vector Double)
+                                                     (Matrix Double)
+
+class Mean m v where
+    mean :: Distr m v -> m
+
+instance Mean Double Double where
+    mean (Gamma s r) = s / r
+
+instance Mean (Vector Double) (Vector Double) where
+    mean (MGamma s r) = s / r
+
+instance Mean (Vector Double) (Matrix Double) where
+  mean (MNormal m v) = m
