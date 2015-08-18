8b8cfe3ba33fef0edba9c83fbe2b18ddaf384611
Danila <ddbihbka@gmail.com>
2015-08-18 18:15:18 +0300

    Add tests for calculated params

--- test/QModels.hs
+++ test/QModels.hs
@@ -6,7 +6,7 @@ import Test.Tasty.HUnit
 import PCA.QModels
 import PCA.Distribution
 import Numeric.LinearAlgebra.Data
-       (fromRows, toRows, size, (!), konst)
+       (fromRows, toRows, size, (!), konst, Matrix, Vector, fromList)
 import qualified Numeric.LinearAlgebra.HMatrix as H
 
 
@@ -44,12 +44,23 @@ unitTests =
                 "Fool impl and matrix impl must give the same result"
                 mMu
                 (foolCalcMMu trainT tau sigmaMu w x)
-            let sigmaW = calcSigmaWM tau alpha x
-            assertEqual "Size of sigma_W is dxd" (size sigmaW) (d,d)
+            let sigmaW = calcSigmaW tau alpha x
+            assertEqual "Size of sigma_W is dxd" (size sigmaW) (d, d)
             assertEqual
                 "Fool impl and matrix impl must give the same result"
                 (foolCalcSigmaW tau alpha x)
                 sigmaW
+            let mW = calcMW tau sigmaW x trainT mu
+            assertEqual "Size of m_W is dxd" (size mW) (d, d)
+            assertEqual
+                "Fool impl and matrix impl of calcMW must give the same result"
+                (foolCalcMW tau sigmaW x trainT mu)
+                mW
+            let bAlpha = calcBalpha (konst 0.001 d) w
+            assertEqual "Size of b_alpha is d" (size bAlpha) d
+            assertEqual "Fool impl and matrix impl of calcBalpha must give the same result" (foolCalcBalpha (konst 0.001 d) w) bAlpha
+            let bTau = calcBtau 0.001 trainT mu w x
+            assertEqual "Fool impl and matrix impl of calcBtau must give the same result" (foolCalcBtau 0.001 trainT mu w x) bTau
 
 
 foolCalcMX trainT tau sigmaX w mu =
@@ -86,3 +97,65 @@ foolCalcSigmaW tau alpha x =
     mX = mean x
     (_,d) = size mX
 
+
+foolCalcMW
+    :: Distr Double Double
+    -> Matrix Double
+    -> Distr (Matrix Double) (Matrix Double)
+    -> Matrix Double
+    -> Distr (Vector Double) (Matrix Double)
+    -> Matrix Double
+foolCalcMW tau sigmaW x trainT mu =
+    fromRows
+        (map
+             (\k ->
+                   (mean tau `H.scale` sigmaW) H.#>
+                   (foldr
+                        (\i resV ->
+                              resV +
+                              ((trainT ! i ! k) - (mMu ! k)) `H.scale` (mX ! i))
+                        (konst 0 d)
+                        [0 .. (n - 1)]))
+                       [0 .. (d - 1)])
+  where
+    (n,d) = size trainT
+    mMu = mean mu
+    mX = mean x
+
+foolCalcBalpha :: Vector Double
+               -> Distr (Matrix Double) (Matrix Double)
+               -> Vector Double
+foolCalcBalpha b w =
+    fromList
+        (map
+             (\i ->
+                   b ! i + (mW ! i) `H.dot` (mW ! i) / 2)
+             [0 .. (d - 1)])
+  where
+    mW = mean w
+    (d,_) = size mW
+
+foolCalcBtau
+    :: Double
+    -> Matrix Double
+    -> Distr (Vector Double) (Matrix Double)
+    -> Distr (Matrix Double) (Matrix Double)
+    -> Distr (Matrix Double) (Matrix Double)
+    -> Double
+foolCalcBtau b trainT mu w x =
+    b +
+    0.5 *
+    sum
+        (map
+             (\i ->
+                   normSq (trainT ! i) + normSq mMu +
+                   2 * (mMu `H.dot` (mW H.#> (mX ! i))) -
+                   2 * (trainT ! i `H.dot` (mW H.#> (mX ! i))) -
+                   2 * (trainT ! i `H.dot` mMu))
+             [0 .. (n - 1)])
+  where
+    normSq v = v `H.dot` v
+    mMu = mean mu
+    mX = mean x
+    mW = mean w
+    (n,_) = size trainT
