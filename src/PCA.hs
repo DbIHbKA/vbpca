e9c98743b6303436fbfcab360ac7a500183606bf
Danila <ddbihbka@gmail.com>
2015-08-18 19:22:21 +0300

    Add fit and transform function. Interface of PCA

--- src/PCA.hs
+++ src/PCA.hs
@@ -1,5 +1,34 @@
 
 module PCA
-    ( 
+    ( fitPCA, transformPCA
     ) where
 
+import PCA.QModels
+import Numeric.LinearAlgebra.Data
+       (Matrix, Vector, toRows, fromRows)
+import qualified Numeric.LinearAlgebra.HMatrix as H
+
+
+-- | Theta parameteres which we will you for predict in model
+data PCAModel = PCAModel
+    { scale :: Matrix Double  -- ^ W
+    , shift :: Vector Double  -- ^ \mu
+    }
+
+
+fitPCA :: Matrix Double -> PCAModel
+fitPCA t =
+    let (w,mu) = calculateQ t
+    in PCAModel
+       { scale = w
+       , shift = mu
+       }
+
+
+transformPCA :: PCAModel -> Matrix Double -> Matrix Double
+transformPCA (PCAModel w mu) t =
+    fromRows
+        (map
+             (\tn ->
+                   tn + mu)
+             (toRows (t H.<> w)))
